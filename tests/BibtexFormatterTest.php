1625c4f8571f6050fd626e3383fcd4c9d0ac480f
Gefrierbrand <nineff@gmail.com>
2014-06-03 14:49:11 +0200

    updateTests (formatterTest needs to be fixed)

--- tests/BibtexFormatterTest.php
+++ tests/BibtexFormatterTest.php
@@ -6,7 +6,7 @@ class BibtexFormatterTest extends \PHPUnit_Framework_TestCase
 {
     public function test_coverage() {
         $instance = new \AudioLabs\BibtexParser\BibtexParser();
-        $items = $instance->parse_file(__DIR__ . '/Fixures/publications.bib');
+        $items = $instance->parseFile(__DIR__ . '/Fixures/publications.bib');
 
         foreach($items as &$item) {
             $formatted = \AudioLabs\BibtexParser\BibtexFormatter::format($item);
--- tests/BibtexParserTest.php
+++ tests/BibtexParserTest.php
@@ -5,7 +5,7 @@ namespace AudioLabs\BibtexParser\Tests;
 class BibtexParserTest extends \PHPUnit_Framework_TestCase
 {
     public function test_coverage() {
-        $items = \AudioLabs\BibtexParser\BibtexParser::parse_file(__DIR__ . '/Fixures/publications.bib');
+        $items = \AudioLabs\BibtexParser\BibtexParser::parseFile(__DIR__ . '/Fixures/publications.bib');
 
         $this->assertTrue(is_array($items));
     }
